05d40adc259a0447ac421d178c0ea1a2b2b1e72d
Ricardo Passos <ricardo.passos.cr@gmail.com>
2021-07-06 10:35:13 -0300

    1 - I deleted unused folders/files (related to firestore) 2 - I updated the backend URLs

--- backend/AthleteProfileService.js
+++ backend/AthleteProfileService.js
@@ -1,10 +1,13 @@
-import { create } from 'apisauce';
+import { create } from "apisauce";
 
 const API = create({
-	baseURL: (process.env.NODE_ENV === 'development') ? `http://${process.env.YOUR_IP}:5003` : "https://athleteprofileservice-dot-secure-sensor-281602.rj.r.appspot.com",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? `http://${process.env.YOUR_IP}:5003`
+      : "https://athleteprofileservice-dot-smartial-315517.rj.r.appspot.com/",
 });
 
 export default async function Post(google_user_id, body) {
-	const apiResponse = await API.post('/' + google_user_id, body);
-	return apiResponse.ok;
+  const apiResponse = await API.post("/" + google_user_id, body);
+  return apiResponse.ok;
 }
--- backend/BasicProfileService.js
+++ backend/BasicProfileService.js
@@ -1,10 +1,13 @@
-import { create } from 'apisauce';
+import { create } from "apisauce";
 
 const API = create({
-	baseURL: (process.env.NODE_ENV === 'development') ? `http://${process.env.YOUR_IP}:5001` : "https://basicprofileservice-dot-secure-sensor-281602.rj.r.appspot.com",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? `http://${process.env.YOUR_IP}:5001`
+      : "https://smartial-315517.rj.r.appspot.com/",
 });
 
 export async function Get(google_user_id) {
-	const apiResponse = await API.get('/' + google_user_id);
-	return apiResponse.ok;
+  const apiResponse = await API.get("/" + google_user_id);
+  return apiResponse.ok;
 }
